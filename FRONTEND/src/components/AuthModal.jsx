448e77645433af73799eac24ee5f204684b74e6e
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-10 00:53:07 +0500

    Update AuthModal.jsx

--- FRONTEND/src/components/AuthModal.jsx
+++ FRONTEND/src/components/AuthModal.jsx
@@ -9,7 +9,8 @@ const AuthModal = () => {
   const dispatch = useDispatch()
     const location = useLocation();
 
-  if (!showAuthModal || location.pathname === "/auth") return null;
+  if (!showAuthModal) return null;
+
 
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-40 flex justify-center items-center">
