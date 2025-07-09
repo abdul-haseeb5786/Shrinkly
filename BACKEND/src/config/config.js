658bc9c1e1001decbe0a5ba89700f58589588b47
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-10 00:37:06 +0500

    Update config.js

--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,7 +1,6 @@
-
 export const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production', 
-    sameSite: 'Lax',
-    maxAge: 1000 * 60 * 60, 
-}
+  httpOnly: true,
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: 'None', // ⬅️ THIS IS THE FIX
+  maxAge: 1000 * 60 * 60, // 1 hour
+};
