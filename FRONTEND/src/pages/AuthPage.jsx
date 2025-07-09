1db3d3be160285dc288dd0f3473acaef4b5d26d7
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-10 00:09:29 +0500

    Add AuthModal display to AuthPage

--- FRONTEND/src/pages/AuthPage.jsx
+++ FRONTEND/src/pages/AuthPage.jsx
@@ -4,20 +4,23 @@ import RegisterForm from '../components/RegisterForm';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from '@tanstack/react-router';
 import { useEffect } from 'react';
+import AuthModal from '../components/AuthModal';
 
 const AuthPage = () => {
     const [login, setLogin] = useState(true);
     const { isAuthenticated } = useSelector((state) => state.auth);
   const navigate = useNavigate();
+   
+  const {showAuthModal} = useSelector((state) => state.auth);
 
      useEffect(() => {
     if (isAuthenticated) {
-      navigate({ to: "/dashboard" }); // ya jo bhi tumhara dashboard path ho
+      navigate({ to: "/dashboard" }); 
     }
   }, [isAuthenticated, navigate]);
     return (
         <div>
-
+{showAuthModal && <AuthModal />}
         
           {  login ? <LoginForm state={setLogin} /> : <RegisterForm state={setLogin} />}
             
