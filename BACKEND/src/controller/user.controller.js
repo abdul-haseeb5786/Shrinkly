62942628903ef6ee97e8d57c9501592bdc79902b
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-09 23:07:07 +0500

    Add getAllUserClicks controller function

--- BACKEND/src/controller/user.controller.js
+++ BACKEND/src/controller/user.controller.js
@@ -1,8 +1,13 @@
-import { getAllUserUrlsDao } from "../dao/user.dao.js";
+import { getAllUserClicksDao, getAllUserUrlsDao } from "../dao/user.dao.js";
 import wrapAsync from "../utils/tryCatchWrapper.js";
 
 export const getAllUserUrls = wrapAsync(async (req, res) => {
     const {_id} = req.user
     const urls = await getAllUserUrlsDao(_id)
   res.status(200).json({message:"succes",urls})
+})
+export const getAllUserClicks = wrapAsync(async (req, res) => {
+    const {_id} = req.user
+    const clicks = await getAllUserClicksDao(_id)
+  res.status(200).json({message:"succes",clicks})
 })
