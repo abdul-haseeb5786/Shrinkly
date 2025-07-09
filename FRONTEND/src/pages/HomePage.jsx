82b12a2b7b0fcd356fbbd9fb73e592766dabfe78
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-10 00:10:32 +0500

    Add AuthModal and update HomePage styling

--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
-
 import React from 'react'
 import UrlForm from '../components/UrlForm'
+import AuthModal from '../components/AuthModal';
+import { useSelector } from 'react-redux';
 
 const HomePage = () => {
+  const {showAuthModal} = useSelector((state) => state.auth);
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-    <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-      <h1 className="text-2xl font-bold text-center mb-6">URL Shortener</h1>
-      <UrlForm/>
+    <div className="min-h-screen bg-[#0d1b2a] flex flex-col items-center justify-center p-4">
+      {showAuthModal && <AuthModal/>}
+      <div className="bg-[#1b263b] p-8 rounded-lg shadow-lg w-full max-w-md">
+        <h1 className="text-2xl font-bold text-[#e0e1dd] text-center mb-6">Shrinkly URL Shortener</h1>
+        <UrlForm />
+      </div>
     </div>
-  </div>
   )
 }
 
