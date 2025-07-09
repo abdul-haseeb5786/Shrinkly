81aa87f9a770bbc3a1e42a25ba1d448292e593f8
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-09 23:07:25 +0500

    Add route for fetching all user clicks

--- BACKEND/src/routes/user.route.js
+++ BACKEND/src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/auth.middleware.js';
-import { getAllUserUrls } from '../controller/user.controller.js';
+import { getAllUserClicks, getAllUserUrls } from '../controller/user.controller.js';
 
 
 const router = express.Router();
 
 router.get('/urls', authMiddleware, getAllUserUrls);  
+router.get('/clicks', authMiddleware, getAllUserClicks);  
 
 
 export default router;
